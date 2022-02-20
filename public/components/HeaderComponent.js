f1c34908476f900bad0a0aefff92650db76baf1a
Daniel Li <zaoster777@gmail.com>
2022-02-19 19:24:15 -0600

    installed reactstrap

---
+++ public/components/HeaderComponent.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'; //importing React and Component module to be able to create components, React is the default export, while Component is a named export
+
+class Header extends Component { //all 
+    
+}
+
+
+export default Header; //exporting the Header component as default as we don't have any other components to export in this file.
