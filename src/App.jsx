1df49af2e9059b02eac2762310b13b31fc5cdb71
Daniel Li <zaoster777@gmail.com>
2022-03-10 10:00:14 -0600

    got array to add elements with every click

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,24 @@ class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            numOfNotes: 0,
-            notesArray: []
-        } //default notes start at 0, and whenever user clicks on header button to add note, this number will increase
+            numOfNotes: 1,
+            notesArray: [0]
+        } //default notes start at 1, whenever the user clicks, there should be at least 1 note, and 1 element in the notesArray
 
         
     }
 
     addNote() { //function for what happens when button is clicked, it seems you create methods outside of the constructor, and they also don't need the function keyword. You just need the name of the function.
         this.setState({numOfNotes: this.state.numOfNotes + 1}); {/*everytime the add note button is clicked, we change numOfNotes property in app to increment by 1*/}
-        this.setState({notesArray: this.state.notesArray.push(this.state.numOfNotes)}); {/*trying something, we are pushing the number of notes into an array so eventually we can map it to create multiple notes*/}
+        
+        /* this.setState({notesArray: this.state.notesArray.push(this.state.numOfNotes)}); */ {/*works for the first button click, but breaks after*/} //I need to figure out why this doesn't work
+
+        this.setState({notesArray: [...this.state.notesArray, this.state.numOfNotes]}); {/*seems like we need to use javascript spread syntax to add a new element into the array. Now we have the array working properly, we can now use map to create multiple notes*/}
+
+        console.log(this.state.numOfNotes);
         console.log(this.state.notesArray);
     }
 
-    /* deleteNotes() {
-        this.setState({numOfNotes: this.state.numOfNotes - 1});
-    } */
     
 
     render() {
@@ -39,11 +41,9 @@ class App extends Component{
     }
 }
 
-/* function ReturnArrayOfNotes() { //trying to return multiple note components depending on the number of notes stores in the state but obviously I'm doing something wrong
-    const arrayNotes = this.state.notesArray.map(number => { //for every number in the array, return a note
-        return <Note/>
-    });
-} */
+function createNotes() {
+
+}
 
 
 export default App;
--- src/components/NoteComponent.jsx
+++ src/components/NoteComponent.jsx
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 
-class Note extends Component {
-    render() {
-        return(
-            <div className='note'>
-                <h1>This is the note title</h1>
-                <p>This is the note content</p>
-            </div>
-        );
-    }
+function Note(props){
+    
+    return(
+        <div className='note'>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
+    );
 }
+
 
 
 export default Note;
