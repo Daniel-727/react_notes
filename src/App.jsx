076e643c7ece92a2329fea17bc60280c0ba8d961
Daniel Li <zaoster777@gmail.com>
2022-04-19 10:52:49 -0500

    fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,21 @@ function App() {
 
     const[notes, setNotes] = React.useState([]); //setting empty array to store notes
 
-    function createNote() { //making notes an array makes sense instead of a variable count, because when I want to add the functionality of deleting a specific note, I can do that using the note's index.
-        let numOfNotes = notes.length;
-        console.log(numOfNotes);
-        setNotes([...notes, numOfNotes]);
+    //note should only have 1 property which is the user input, we're going to call it text
+    const[note, setNote] = React.useState({
+        text: ''
+    }); //setting note to be an object with an empty string property called text
+
+    function createNote() { //now with createNote we want to add an empty note into the notes array
+        setNotes([...notes, note]);
     }
 
-    function deleteNote(event,id) { //this should return all the Notes where the index is not equal to id, but it's just deleting the last Note. Maybe because I didn't have the event
-        console.log(event.timeStamp);
+    function deleteNote(id) { //this should return all the Notes where the index is not equal to id, but it's just deleting the last Note. Maybe because I didn't have the event
+        //I think the deleteNote function isn't working as intended because the notes array is literally an array of integers, when we filter an index and delete an index, it's holding just an integer which is not reflective of the note object we're trying to delete. So the solution to this is probably to create an object and actually store the object in notes instead of some integers. The notes object will by default just have empty strings in which the user can then type into.
+
         setNotes(notes => {
             return notes.filter((e,i) => { //this will return an array of notes where the note that is clicked
-                return i !== id;
+                return id !== i;
             });
         });
     }
@@ -27,9 +31,11 @@ function App() {
     return (
         <>
             <Header addNote={createNote}/>
-            {notes.map((e,i) => {
-                return <Note key={i} id={i} clickX={deleteNote}/>
-            })}
+            <div className='row'>
+                {notes.map((e,i) => {
+                    return <Note key={i} id={i} clickX={deleteNote} userInput={setNote}/>
+                })}
+            </div>
             <Footer/> 
         </>
     );
--- src/components/NoteComponent.jsx
+++ src/components/NoteComponent.jsx
@@ -15,13 +15,23 @@ function Note(props){
     }
 
     function handleClick(){
+        /* console.log('hi'); */
+        props.clickX(props.id); // this is the deleteNote() function passed in from the App component
+    }
 
+    function handleChange(e){
+        console.log('hi');
+        props.userInput({text: e.target.value}
+        );
     }
     
     return( 
-        <div className='col'>
+        <div className='col-lg-3 col-md-4 col-sm-6'>
             <div className='note' onMouseOver={revealX} onMouseLeave={hideX}> {/*when user is hovering mouse over the Note, then we want the x to be visible so the user can click on it and delete the note if they want. Otherwise, the x should be hidden*/}
-                <span contentEditable='true'></span>
+                <div className='col'>
+                    <input name='title' onChange={handleChange}></input>
+                    <input name='content' onChange={handleChange}></input> {/*onChange event listener doesn't work with spans, but it works with inputs*/}
+                </div>
                 {hover ? <div onClick={handleClick} className='xMark'><FontAwesomeIcon icon={faXmark} /></div> : <div></div>}
             </div>
         </div> 
